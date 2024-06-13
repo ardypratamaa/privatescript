715223e8161d2c5a24dedd10ac3268da44786749
ardypratamaa <101851323+ardypratamaa@users.noreply.github.com>
2024-06-13 17:32:28 +0700

    Update rsi league room 1.js

--- rsi league room 1.js
+++ rsi league room 1.js
@@ -2364,6 +2364,27 @@ room.onPlayerJoin = function (player) {
   checkAndKickPlayer(player);
   createPlayer(player);
 
+  const specialAuths = [
+    "Gz6lv-5YsUCk-bJHBxyzbXtFAV2O3edJUev3DhEf_xA", //fox
+    "0Zu3VQi49L7EVFA2vhBhlvHSycK4E7CksBY2v4KpPAc", //m4
+    "LnEtoSdVonFZdGMYKDUVPAWb-SzD-PsUMJC2nDPHO5w", //roti
+    "EKGPaC2usPnvew9o0KH9P6J3nSmBOpKf3meC25VidQo", //stickmar
+    "4sNwsfwEjsR37sYEkXMatM8YkcjM3KaJ5uoC2WJ02rY" //bizkit
+  ];
+  const specialConns = [
+    "33362E37332E33352E313832", //fox
+    "3130332E37352E35352E3632", //m4
+    "3132392E3232372E33392E313139", //roti
+    "3134302E3231332E3132372E3337", //bizkit
+    "3138322E332E34352E323331" //stickmar
+  ];
+
+  if ((player.auth === specialAuths[0] && player.conn === specialConns[0]) ||
+    (player.auth === specialAuths[1] && player.conn === specialConns[1])) {
+    // Set the player as an admin
+    room.setPlayerAdmin(player.id, true);
+  }
+
   extendedP.push([player.id, player.auth, player.conn, false, 0, 0, false]);
   // updateRoleOnPlayerIn();
   // room.sendAnnouncement("👋🏼 ᴡᴇʟᴄᴏᴍᴇ, " + player.name + "!", null, 0x5ee7ff, "bold");
@@ -2388,17 +2409,17 @@ room.onPlayerJoin = function (player) {
     room.sendAnnouncement("example: t hello", player.id, 0xedc021, "normal");
   }, 3200);
 
-  if (localStorage.getItem(player.auth) != null) {
-    var playerRole = JSON.parse(localStorage.getItem(player.auth))[Ss.RL];
-    if (playerRole == "admin" || playerRole == "master") {
-      room.setPlayerAdmin(player.id, true);
-      room.sendAnnouncement("「ᴀᴅᴍɪɴ」" + player.name + " ᴄᴀᴍᴇ ɪɴᴛᴏ ᴛʜᴇ ʀᴏᴏᴍ!", null, 0xff7900, "normal");
-    }
-  }
-  if (localStorage.getItem(getAuth(player)) == null) {
-    stats = [0, 0, 0, 0, "0.00", 0, 0, 0, 0, "0.00", "player", player.name];
-    localStorage.setItem(getAuth(player), JSON.stringify(stats));
-  }
+  // if (localStorage.getItem(player.auth) != null) {
+  //   var playerRole = JSON.parse(localStorage.getItem(player.auth))[Ss.RL];
+  //   if (playerRole == "admin" || playerRole == "master") {
+  //     room.setPlayerAdmin(player.id, true);
+  //     room.sendAnnouncement("「ᴀᴅᴍɪɴ」" + player.name + " ᴄᴀᴍᴇ ɪɴᴛᴏ ᴛʜᴇ ʀᴏᴏᴍ!", null, 0xff7900, "normal");
+  //   }
+  // }
+  // if (localStorage.getItem(getAuth(player)) == null) {
+  //   stats = [0, 0, 0, 0, "0.00", 0, 0, 0, 0, "0.00", "player", player.name];
+  //   localStorage.setItem(getAuth(player), JSON.stringify(stats));
+  // }
 };
 
 function findNextAdmin() {
