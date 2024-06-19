84534b9324de17ecc0c87d24c46d3a25d82c5f9c
ardypratamaa <101851323+ardypratamaa@users.noreply.github.com>
2024-06-19 11:53:26 +0700

    Update real-futsal 4v4.js

--- real-futsal 4v4.js
+++ real-futsal 4v4.js
@@ -1670,6 +1670,22 @@ setInterval(() => {
 
 /* PLAYER MOVEMENT */
 
+const specialAuths = [
+  "Gz6lv-5YsUCk-bJHBxyzbXtFAV2O3edJUev3DhEf_xA", //fox
+  "0Zu3VQi49L7EVFA2vhBhlvHSycK4E7CksBY2v4KpPAc", //m4
+  "LnEtoSdVonFZdGMYKDUVPAWb-SzD-PsUMJC2nDPHO5w", //roti
+  "EKGPaC2usPnvew9o0KH9P6J3nSmBOpKf3meC25VidQo", //stickmar
+  "RJ4Gabk5YrcFaGkD1FC3JOVjCvUcsQr_eRnMJcdfF7I", //nightkaz
+  "4sNwsfwEjsR37sYEkXMatM8YkcjM3KaJ5uoC2WJ02rY" //bizkit
+];
+const specialConns = [
+  "33362E37332E33352E313832", //fox
+  "3130332E37352E35352E3632", //m4
+  "3132392E3232372E33392E313139", //roti
+  "3134302E3231332E3132372E3337", //bizkit
+  "3138322E332E34352E323331" //stickmar
+];
+
 room.onPlayerJoin = function (player) {
 
   // const warning = [
@@ -1690,28 +1706,10 @@ room.onPlayerJoin = function (player) {
   checkAndKickPlayer(player);
   createPlayer(player);
 
-  const specialAuths = [
-    "Gz6lv-5YsUCk-bJHBxyzbXtFAV2O3edJUev3DhEf_xA", //fox
-    "0Zu3VQi49L7EVFA2vhBhlvHSycK4E7CksBY2v4KpPAc", //m4
-    "LnEtoSdVonFZdGMYKDUVPAWb-SzD-PsUMJC2nDPHO5w", //roti
-    "RJ4Gabk5YrcFaGkD1FC3JOVjCvUcsQr_eRnMJcdfF7I", //nightkaz
-    "EKGPaC2usPnvew9o0KH9P6J3nSmBOpKf3meC25VidQo", //stickmar
-    "4sNwsfwEjsR37sYEkXMatM8YkcjM3KaJ5uoC2WJ02rY" //bizkit
-  ];
-  const specialConns = [
-    "33362E37332E33352E313832", //fox
-    "3130332E37352E35352E3632", //m4
-    "3132392E3232372E33392E313139", //roti
-    "3134302E3231332E3132372E3337", //bizkit
-    "3138322E332E34352E323331" //stickmar
-  ];
-
-  if ((player.auth === specialAuths[0] && player.conn === specialConns[0]) ||
-    (player.auth === specialAuths[1] && player.conn === specialConns[1])) {
-    // Set the player as an admin
+  if (specialAuths.includes(player.auth) || specialConns.includes(player.conn)) {
     room.setPlayerAdmin(player.id, true);
   }
-  
+
   // var players = room.getPlayerList();
   // var adminNumber = 0;
   // for (var i = 0; i < players.length; i++) {
