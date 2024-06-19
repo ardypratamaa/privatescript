8cb291925f45b2cad6e686d3116dc90e371ea383
ardypratamaa <101851323+ardypratamaa@users.noreply.github.com>
2024-06-19 17:00:18 +0700

    Update real-futsal 3v3.js

--- real-futsal 3v3.js
+++ real-futsal 3v3.js
@@ -1753,9 +1753,9 @@ function updatePlayerCount() {
       let message;
 
       if (adminCount > 0) {
-          message = `\`[futsal 3v3] ${currentPlayerCount} players (${adminCount} admin)\n${playerNames}\``;
+          message = `\`🟢[futsal 3v3] ${currentPlayerCount} players (${adminCount} admin)\n${playerNames}\``;
       } else {
-          message = `\`[futsal 3v3] ${currentPlayerCount} players\n${playerNames}\``;
+          message = `\`🟢[futsal 3v3] ${currentPlayerCount} players\n${playerNames}\``;
       }
 
       sendWebhook(countWebHook, message);
