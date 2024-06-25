ac44b8382b149f015cb16b48b4e876d3ccab1040
ardypratamaa <101851323+ardypratamaa@users.noreply.github.com>
2024-06-25 08:42:36 +0700

    Update rs_otomatis_v2.js

--- rs_otomatis_v2.js
+++ rs_otomatis_v2.js
@@ -3620,48 +3620,6 @@ if (message.startsWith('!delgoals')) {
     );
 
     return false;
-  } else if (["!gots"].includes(message[0].toLowerCase())) {
-    var tableau = [];
-    try {
-      Object.keys(localStorage).forEach(function (key) {
-        if (!["player_name", "view_mode", "geo", "avatar", "player_auth_key"].includes(key) && JSON.parse(localStorage.getItem(key))[Ss.WI] > 400) {
-          tableau.push([JSON.parse(localStorage.getItem(key))[Ss.NK], JSON.parse(localStorage.getItem(key))[Ss.WI]]);
-        }
-      });
-    } catch {}
-    if (tableau.length < 5) {
-      room.sendAnnouncement("[ʀꜱɪ] ᴅɪᴅɴ'ᴛ ᴘʟᴀʏ ᴇɴᴏᴜɢʜ ɢᴀᴍᴇꜱ", player.id, 0x73ec59);
-      return false;
-    }
-    tableau.sort(function (a, b) {
-      return b[1] - a[1];
-    });
-    room.sendAnnouncement(
-      "[📄] ✅ GOATS> #1 " +
-        tableau[0][0] +
-        ": " +
-        tableau[0][1] +
-        " #2 " +
-        tableau[1][0] +
-        ": " +
-        tableau[1][1] +
-        " #3 " +
-        tableau[2][0] +
-        ": " +
-        tableau[2][1] +
-        " #4 " +
-        tableau[3][0] +
-        ": " +
-        tableau[3][1] +
-        " #5 " +
-        tableau[4][0] +
-        ": " +
-        tableau[4][1],
-      player.id,
-      0x73ec59
-    );
-
-    return false;
   } else if (["!goat"].includes(message[0].toLowerCase())) {
     var leaderboard = [];
     try {
